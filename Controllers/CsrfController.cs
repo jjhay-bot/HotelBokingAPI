7821e5b8a3dbeb9cda9e283cd2b9521cd5042719
jhay <jhay.alcorcon@gmail.com>
2025-10-04 20:13:31 +0800

    added CSFT sessions for state-changing requests

---
+++ Controllers/CsrfController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Cryptography;
+using System;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/csrf-sessions")]
+    public class CsrfController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetCsrfToken()
+        {
+            // Generate a random CSRF token
+            var tokenBytes = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(tokenBytes);
+            }
+            var csrfToken = Convert.ToBase64String(tokenBytes);
+
+            // Set CSRF token in a non-HttpOnly, Secure cookie
+            var jwtSettings = HttpContext.RequestServices.GetService<IConfiguration>()?.GetSection("Jwt");
+            var expires = DateTime.UtcNow.AddMinutes(int.Parse(jwtSettings?["ExpiresInMinutes"] ?? "60"));
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = false, // Must be readable by JS
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = expires
+            };
+            Response.Cookies.Append("XSRF-TOKEN", csrfToken, cookieOptions);
+
+            // Return token in response body as well
+            return Ok(new { csrfToken });
+        }
+    }
+}
